d433e94e0697e67f7bea1c971b065fc993a835c7
davidmoral22 <davidml0612@gmail.com>
2026-04-10 17:36:09 +0200

    Punto 4: Añadida documentación general del proyecto

--- src/miniCalculadora.java
+++ src/miniCalculadora.java
@@ -1,3 +1,10 @@
+/**
+ * Esta clase proporciona métodos para realizar operaciones matemáticas simples
+ * utilizando la clase Math de Java.
+ * * @author David
+ * @version 1.0
+ */
+
 public class miniCalculadora {
 
     public static double valorAbsoluto(double numero) {
