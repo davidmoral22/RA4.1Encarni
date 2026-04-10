d18a3506915786588815a2ce41d998332d757915
davidmoral22 <davidml0612@gmail.com>
2026-04-10 17:36:33 +0200

    Punto 5: Añadida documentación de valorAbsoluto

--- src/miniCalculadora.java
+++ src/miniCalculadora.java
@@ -7,6 +7,12 @@
 
 public class miniCalculadora {
 
+    /**
+     * Calcula el valor absoluto de un número decimal.
+     * * @param numero El número del cual se desea obtener el valor absoluto.
+     * @return El valor absoluto del número proporcionado.
+     */
+
     public static double valorAbsoluto(double numero) {
         return Math.abs(numero);
     }
